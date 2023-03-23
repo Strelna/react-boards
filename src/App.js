ebd74cb1bed219a55059a3c160459511ad254095
Strelna <aleesartshop@gmail.com>
2023-03-23 17:53:03 +0200

    added logo pic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+        <img src="/img/logo.png" />
+        <div className="headerInfo">
+          <h3>React Sneakers</h3>
+          <p>Shop of the best shoes</p>
+        </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <svg />
+            <span>50 USD</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>All shoes</h1>
+        ........
+      </div>
     </div>
   );
 }
