3d28e95ad528497e9fab5a740a774eec2b8b66b6
Strelna <aleesartshop@gmail.com>
2023-05-02 17:43:06 +0300

    created Card folder

--- src/App.js
+++ src/App.js
@@ -42,7 +42,12 @@ function App() {
         </div>
         <div className="d-flex">
           {arr.map((obj) => (
-            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
           ))}
         </div>
       </div>
